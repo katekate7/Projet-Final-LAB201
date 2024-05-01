58aff5992ab5c40c92caef045a25c19d10f782cb
Kate <katrinper6@gmail.com>
2024-05-01 23:31:08 +0200

    update all file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,14 +6,13 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCkKoUE1QUrjqh7N5P1s8b3uq4ZYgPHpdw",
-  authDomain: "projet-final-lab201.firebaseapp.com",
-  databaseURL: "https://projet-final-lab201-default-rtdb.firebaseio.com",
-  projectId: "projet-final-lab201",
-  storageBucket: "projet-final-lab201.appspot.com",
-  messagingSenderId: "73379607362",
-  appId: "1:73379607362:web:bdebb7876aaa30e4360edb",
-  measurementId: "G-67X9FNSX5G"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
